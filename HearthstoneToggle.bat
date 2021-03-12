ad65ea8e64f3d02476bca45a7fb8a5d7392dbed7
Stuart-McCreight <68667066+Stuart-McCreight@users.noreply.github.com>
2021-03-12 12:45:07 -0500

    Add files via upload

---
+++ HearthstoneToggle.bat
@@ -0,0 +1,7 @@
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || ( echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B ) 
+
+netsh advfirewall firewall set rule name=hearthstone new enable=yes
+timeout /t 3
+netsh advfirewall firewall set rule name=hearthstone new enable=no 
+exit										
